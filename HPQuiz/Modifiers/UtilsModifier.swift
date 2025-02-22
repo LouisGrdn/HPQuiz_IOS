dc71262f3cc5edd62e547604d0bb684c687df14f
Louis Gardin <louis.gardin05@gmail.com>
2025-02-22 01:37:03 +0100

    Add cornerRadius global modifier

--- HPQuiz/Modifiers/UtilsModifier.swift
+++ HPQuiz/Modifiers/UtilsModifier.swift
@@ -5,4 +5,21 @@
 //  Created by Louis Gardin on 22/02/2025.
 //
 
-import Foundation
+import SwiftUI
+
+struct CornerRadius: ViewModifier {
+    var radius: CGFloat
+    func body(content: Content) -> some View {
+        content
+            .clipShape(RoundedRectangle(cornerRadius: radius))
+            .overlay(RoundedRectangle(cornerRadius: radius)
+                .stroke(.black, lineWidth: 2)
+                )
+    }
+}
+
+extension View {
+    public func cornerRadius(radius: CGFloat) -> some View {
+        self.modifier(CornerRadius(radius: radius))
+    }
+}
